2e166e5c83408e6d104f4495eac7cd29b34295eb
SHUBHAM <SHUBHAM@SHUBHAM>
2020-03-01 12:15:06 +0530

    Added pojos package and CustInfo class in this package.

---
+++ Sample1/src/pojos/CustInfo.java
@@ -0,0 +1,35 @@
+package pojos;
+
+public class CustInfo {
+
+	int id;
+	String name;
+
+	public CustInfo(int id, String name) {
+
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "CustInfo [id=" + id + ", name=" + name + "]";
+	}
+
+}
